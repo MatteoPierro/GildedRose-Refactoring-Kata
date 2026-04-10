230195657f466cc9eebd48908be1849576323a6e
Alex Thomson <alex.thomson@prima.it>
2026-04-10 14:02:01 +0100

    refactor brie branches

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -34,57 +34,70 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for i in 0..self.items.len() {
-            if self.items[i].name != "Aged Brie"
-                && self.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-            {
-                if self.items[i].quality > 0 {
-                    if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        self.items[i].quality = self.items[i].quality - 1;
+            if self.items[i].name == "Aged Brie" {
+                {
+                    if self.items[i].quality < 50 {
+                        self.items[i].quality = self.items[i].quality + 1;
                     }
-                }
+
+                    self.items[i].sell_in = self.items[i].sell_in - 1;
+
+                    if self.items[i].sell_in < 0 {
+                        if self.items[i].quality < 50 {
+                            self.items[i].quality = self.items[i].quality + 1;
+                        }
+                    }
+                };
             } else {
-                if self.items[i].quality < 50 {
-                    self.items[i].quality = self.items[i].quality + 1;
-
-                    if self.items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if self.items[i].sell_in < 11 {
-                            if self.items[i].quality < 50 {
-                                self.items[i].quality = self.items[i].quality + 1;
-                            }
-                        }
-
-                        if self.items[i].sell_in < 6 {
-                            if self.items[i].quality < 50 {
-                                self.items[i].quality = self.items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
-                self.items[i].sell_in = self.items[i].sell_in - 1;
-            }
-
-            if self.items[i].sell_in < 0 {
-                if self.items[i].name != "Aged Brie" {
-                    if self.items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+                {
+                    if self.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+                    {
                         if self.items[i].quality > 0 {
                             if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
                                 self.items[i].quality = self.items[i].quality - 1;
                             }
                         }
                     } else {
-                        self.items[i].quality = self.items[i].quality - self.items[i].quality;
+                        if self.items[i].quality < 50 {
+                            self.items[i].quality = self.items[i].quality + 1;
+
+                            if self.items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+                                if self.items[i].sell_in < 11 {
+                                    if self.items[i].quality < 50 {
+                                        self.items[i].quality = self.items[i].quality + 1;
+                                    }
+                                }
+
+                                if self.items[i].sell_in < 6 {
+                                    if self.items[i].quality < 50 {
+                                        self.items[i].quality = self.items[i].quality + 1;
+                                    }
+                                }
+                            }
+                        }
                     }
-                } else {
-                    if self.items[i].quality < 50 {
-                        self.items[i].quality = self.items[i].quality + 1;
+
+                    if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
+                        self.items[i].sell_in = self.items[i].sell_in - 1;
                     }
-                }
+
+                    if self.items[i].sell_in < 0 {
+                        if self.items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+                            if self.items[i].quality > 0 {
+                                if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
+                                    self.items[i].quality = self.items[i].quality - 1;
+                                }
+                            }
+                        } else {
+                            self.items[i].quality = self.items[i].quality - self.items[i].quality;
+                        }
+                    }
+                };
             }
         }
     }
+
+    
 }
 
 #[cfg(test)]
